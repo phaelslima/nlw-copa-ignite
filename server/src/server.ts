2a101f6908ac67084f93f82ca8ab19cfaf40c113
Raphael Lima <phaelslima@gmail.com>
2022-11-24 22:36:50 -0300

    feat(server): add route - GET /polls/count

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,19 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
+  })
+
+  fastify.get('/polls/count', async () => {
+    const count = await prisma.poll.count()
+
+    return { count }
   })
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
