cc7ee50f06ac436ade1d6e30ad988b604b537a92
Raphael Lima <phaelslima@gmail.com>
2022-11-26 11:48:28 -0300

    feat(mobile): configure statusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar'
-import { Center, NativeBaseProvider, Text } from 'native-base'
+import { Center, NativeBaseProvider, StatusBar, Text } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading'
@@ -11,11 +10,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       <Center flex={1} bg="gray.900">
         {fontsLoaded ? <Text>Hello world!</Text> : <Loading />}
       </Center>
-
-      <StatusBar style="auto" />
     </NativeBaseProvider>
   )
 }
