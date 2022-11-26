ac9d25cc23e97ac5fac1f2223b3c5e4e2331cdf9
Raphael Lima <phaelslima@gmail.com>
2022-11-26 11:46:33 -0300

    feat(mobile): add loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
-import { Center, NativeBaseProvider, Spinner, Text } from 'native-base'
+import { Center, NativeBaseProvider, Text } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from './src/components/Loading'
 
 import { THEME } from './src/styles/theme'
 
@@ -10,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <Center flex={1} bg="gray.900">
-        {fontsLoaded ? <Text>Hello world!</Text> : <Spinner color="yellow.500" />}
+        {fontsLoaded ? <Text>Hello world!</Text> : <Loading />}
       </Center>
 
       <StatusBar style="auto" />
