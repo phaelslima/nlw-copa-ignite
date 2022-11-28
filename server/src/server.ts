189fe7781a18b5e9e7d6c4cb9fd0ca7016aa1246
Raphael Lima <phaelslima@gmail.com>
2022-11-27 22:01:41 -0300

    feat(server): add route - GET /users/count

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,6 +44,12 @@ async function bootstrap() {
     return reply.status(201).send({ code })
   })
 
+  fastify.get('/users/count', async () => {
+    const usersCount = await prisma.user.count()
+
+    return { count: usersCount }
+  })
+
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
