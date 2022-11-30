b404bb5d08c47ea23c64f38fb9b2076292d4ad45
Raphael Lima <phaelslima@gmail.com>
2022-11-30 19:34:13 -0300

    feat(server): add route - POST /polls/join

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -3,6 +3,7 @@ import ShortUniqueId from 'short-unique-id'
 import { z } from 'zod'
 
 import { prisma } from '../lib/prisma'
+import { authenticate } from '../plugins/authenticate'
 
 export async function pollRoutes(fastify: FastifyInstance) {
   fastify.get('/polls/count', async () => {
@@ -49,4 +50,59 @@ export async function pollRoutes(fastify: FastifyInstance) {
 
     return reply.status(201).send({ code })
   })
+
+  fastify.post(
+    '/polls/join',
+    {
+      onRequest: [authenticate],
+    },
+    async (request, reply) => {
+      const joinPollBody = z.object({
+        code: z.string(),
+      })
+
+      const { code } = joinPollBody.parse(request.body)
+
+      const poll = await prisma.poll.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      })
+
+      if (!poll) {
+        return await reply.status(404).send({ message: 'Poll not found.' })
+      }
+
+      if (poll.participants.length > 0) {
+        return await reply.status(409).send({ message: 'Already joined.' })
+      }
+
+      if (!poll.ownerId) {
+        await prisma.poll.update({
+          where: {
+            id: poll.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        })
+      }
+
+      await prisma.participant.create({
+        data: {
+          pollId: poll.id,
+          userId: request.user.sub,
+        },
+      })
+
+      return await reply.status(201).send()
+    }
+  )
 }
