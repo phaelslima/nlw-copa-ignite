9925de9526e77c07270f8a12f5e277e72e0237ca
Raphael Lima <phaelslima@gmail.com>
2022-12-01 21:56:06 -0300

    feat(mobile): configure sign in with google

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -3,9 +3,13 @@ import { Fontisto } from '@expo/vector-icons'
 
 import { Button } from '../components/Button'
 
+import { useAuth } from '../hooks/useAuth'
+
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -15,6 +19,7 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
         mt={12}
+        onPress={signIn}
       />
 
       <Text
