ec40416066f67ee708b9990665d3741f09bb7f9b
Raphael Lima <phaelslima@gmail.com>
2022-12-04 22:57:47 -0300

    feat(mobile): add polls list

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,12 +1,46 @@
-import { Icon, VStack } from 'native-base'
+import { FlatList, Icon, useToast, VStack } from 'native-base'
 import { Octicons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
+import { api } from '../libs/axios'
+import { useEffect, useState } from 'react'
+
+import { PollCard, PollCardPros } from '../components/PollCard'
+import { EmptyPollList } from '../components/EmptyPollList'
+import { Loading } from '../components/Loading'
 
 export function Polls() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [polls, setPolls] = useState<PollCardPros[]>([])
+  
   const { navigate } = useNavigation()
+
+  const toast =  useToast()
+
+  async function fetchPolls() {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get('/polls')
+      setPolls(response.data.polls)
+    } catch (error) {
+      console.log(error)
+      
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchPolls()
+  }, [])
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -19,6 +53,19 @@ export function Polls() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {isLoading
+        ? <Loading />
+        : <FlatList
+            data={polls}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <PollCard data={item} />}
+            ListEmptyComponent={() => <EmptyPollList />}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+          />
+      }
     </VStack>
   )
 }
