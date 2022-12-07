7df585389e6634368970cc7ef1cf7f55a4909e87
Raphael Lima <phaelslima@gmail.com>
2022-12-06 23:23:19 -0300

    feat(mobile): add poll details

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { New } from '../screens/New'
 import { Polls } from '../screens/Polls'
 import { Find } from '../screens/Find'
+import { Details } from '../screens/Datails'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
@@ -54,6 +55,14 @@ export function AppRoutes() {
       <Screen
         name="find"
         component={Find}
+        options={{
+          tabBarButton: () => null
+        }}
+      />
+
+      <Screen
+        name="details"
+        component={Details}
         options={{
           tabBarButton: () => null
         }}
